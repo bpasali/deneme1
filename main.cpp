987d097025cbefb193652736d1c306ab658d6cbb
Burak Paşalı <Burak Paal>
2016-10-19 13:58:25 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int mult (int x, int y)
+{
+
+
+    return (x * y);
+}
+int e(int n,int k = 2 )
+{
+    if (k == 2)
+    return (n * n );
+
+    else
+        return (mult(n,k-1)*n);
+
+
+}
+int main()
+{
+
+    cout<<e(5,3)<< endl;
+
+    return 0;
+
+
+
+
+}
+
+//Ders:             1
+//Alıştırma:        2
